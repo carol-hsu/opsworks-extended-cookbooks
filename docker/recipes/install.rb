f79159e0f63c4bc5c32450c7123819786ff7d281
carol-hsu <asymplone@gmail.com>
2015-07-01 18:32:51 +0800

    docker 1.7

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -4,17 +4,24 @@ when "ubuntu","debian"
     action :install
   end
 when 'centos','redhat','fedora','amazon'
-  package "docker" do
-    action :install
-  end
-  bash "upgrate_docker" do
-	user "root"
-	code <<-EOH
-		mv /usr/bin/docker /usr/bin/docker-old
-		wget https://get.docker.com/builds/Linux/x86_64/docker-latest -O /usr/bin/docker
-		chmod 777 /usr/bin/docker
-	EOH
-  end
+ # package "docker" do
+ #   action :install
+ # end
+ # bash "upgrate_docker" do
+ #   user "root"
+ #   code <<-EOH
+ #   	mv /usr/bin/docker /usr/bin/docker-old
+ #   	wget https://get.docker.com/builds/Linux/x86_64/docker-latest -O /usr/bin/docker
+ #   	chmod 777 /usr/bin/docker
+ #   EOH
+ # end
+	bash "install-docker" do
+		user 'root'
+		code <<-EOH
+		curl -O -sSL https://get.docker.com/rpm/1.7.0/centos-6/RPMS/x86_64/docker-engine-1.7.0-1.el6.x86_64.rpm
+		yum -y localinstall --nogpgcheck docker-engine-1.7.0-1.el6.x86_64.rpm
+		EOH
+	end
 end
 
 service "docker" do
