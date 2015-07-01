9820c2bb18a44042070b0c409e4ec2f6ca8e0012
carol-hsu <asymplone@gmail.com>
2015-07-01 18:48:45 +0800

    update

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -18,6 +18,7 @@ when 'centos','redhat','fedora','amazon'
 	bash "install-docker" do
 		user 'root'
 		code <<-EOH
+		yum -y update
 		curl -O -sSL https://get.docker.com/rpm/1.7.0/centos-6/RPMS/x86_64/docker-engine-1.7.0-1.el6.x86_64.rpm
 		yum -y localinstall --nogpgcheck docker-engine-1.7.0-1.el6.x86_64.rpm
 		EOH
